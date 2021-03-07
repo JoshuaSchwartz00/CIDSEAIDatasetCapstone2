50cebd5ed4748047efca30a5c4d8d5f1475acdcc
Wei Chen <wchen147@sundevils.asu.edu>
2021-03-06 22:43:41 -0700

    Update main.py
    able to generate 4672 images
    Twisted a little bit of the capture frame

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from vpython import *
 from random import shuffle, randint
 
 list_scenes = list()
+scenes_count = 1  # to store the file number
 
 # Press the green button in the gutter to run the script.
 class objects:
@@ -81,6 +82,42 @@ def isOverlap(object_list, obj):
         if colors == colors and size == size1 and shape == shape1:
             return True
     return False
+    
+
+def draw_objects(obj_in_the_sence, list_object):
+    this_obj = permutation_list[randint(0, len(permutation_list) - 1)]
+
+    while isOverlap(obj_in_the_sence, this_obj):
+        this_obj = permutation_list[randint(0, len(permutation_list) - 1)]
+
+    (colors, size, shape, pos) = this_obj
+    obj_in_the_sence.append(this_obj)
+    (pos_x, pos_y, pos_z) = pos
+    object_1 = objects(size=size, location=pos, shape=shape, colors=colors)
+    # To store the object in a list
+    list_object.append(object_1)
+    # Draw shapes
+    if shape == "box":
+        draw_box(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
+    else:
+        draw_sphere(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
+    return obj_in_the_sence, list_object
+
+
+def capture_image(list_object, sence):
+    # =====  capture the Scenes =====
+    global scenes_count
+    time.sleep(0.15)
+    im = ImageGrab.grab((8, 95, 505, 570))
+    filename = "object{num}.jpg".format(num=scenes_count)
+    scenes_count += 1
+    im.save(filename)
+    # storing data to objects
+    scene_obj = scene(filename, list_object)
+    list_scenes.append(scene_obj)
+    # reset the canvas
+    canvas.delete(self=sence)
+
 
 if __name__ == '__main__':
     # =====  Generating permutation list  =====
@@ -88,82 +125,20 @@ if __name__ == '__main__':
                 [(-2, -2, 0), (-2, 0, 0), (-2, 2, 0), (0, -2, 0), (0, 0, 0), (0, 2, 0), (2, -2, 0), (2, 0, 0), (2, 2, 0)]]
     permutation_list = list(itertools.product(*all_list))
     shuffle(permutation_list)  # To shuffle the list
-    scenes_count = 1  # to store the file number
+    
+# ===== Make scenes of objects  =====
+    for idx1 in range(0, (8*8*8*8+8*8*8+8*8)):
+        sence = canvas(width=500, height=500)
+        list_object = list()
+        obj_in_the_sence = list()
+        (obj_in_the_sence, list_object) = draw_objects(obj_in_the_sence, list_object)
+        (obj_in_the_sence, list_object) = draw_objects(obj_in_the_sence, list_object)
+        if idx1 >= 64:
+            (obj_in_the_sence, list_object) = draw_objects(obj_in_the_sence, list_object)
+        if idx1 >= 576:
+            (obj_in_the_sence, list_object) = draw_objects(obj_in_the_sence, list_object)
+        capture_image(list_object, sence)
 
-    # =====  Testing area =====
-    # print(str(permutation_list))
-    # inner_idx = 0
-    # num_of_obj = 2
-    # idx = 0
-    # sence = canvas(width=500, height=500)
-    # while inner_idx < num_of_obj:
-    #     (colors, size, shape, pos) = permutation_list[idx + inner_idx]
-    #     (pos_x, pos_y, pos_z) = pos
-    #     print(str(permutation_list[idx + inner_idx]))
-    #     object_1 = objects(size=size, location=pos, shape=shape, colors=colors)
-    #     # To store the object in a list
-    #     # list_object.append(object_1)
-    #     if shape == "box":
-    #         draw_box(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
-    #     else:
-    #         draw_sphere(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
-    #     inner_idx += 1
-    # (pos, colors, size, shape) = permutation_list[1]
-    # (pos_x, pos_y, pos_z) = pos
-    # print(str(permutation_list[1]))
-    # print(str(pos))
-    # object_1 = objects(size=size, location=pos, shape=shape, colors=colors)
-    # sence = canvas(width=500, height=500)
-
-    # draw_box(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
-    # print("length of list", len(permutation_list))
-
-    # ===== Make scenes of objects  =====
-    idx = 0  # index of permutation list
-    for num_of_obj in range(2, 5): # number of objects from 2 to 4
-        for x_1 in range(num_of_obj):
-            idx = x_1
-            while idx < len(permutation_list):
-                sence = canvas(width=500, height=500)
-                list_object = list()
-                inner_idx = 0
-                obj_in_the_sence = list()
-                while inner_idx < num_of_obj:
-                    if (idx + inner_idx) < (len(permutation_list) - 1):
-                        this_obj = permutation_list[idx + inner_idx]
-                    else:
-                        this_obj = permutation_list[randint(0, len(permutation_list) - 1)]
-
-                    # To check if the position is overlapped
-                    # If overlap return ture pick, then a random obj from permutation list
-                    while isOverlap(obj_in_the_sence, this_obj):
-                        this_obj = permutation_list[randint(0, len(permutation_list) - 1)]
-
-                    (colors, size, shape, pos) = this_obj
-                    obj_in_the_sence.append(this_obj)
-                    (pos_x, pos_y, pos_z) = pos
-                    object_1 = objects(size=size, location=pos, shape=shape, colors=colors)
-                    # To store the object in a list
-                    list_object.append(object_1)
-                    # Draw shapes
-                    if shape == "box":
-                        draw_box(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
-                    else:
-                        draw_sphere(position=vector(pos_x, pos_y, pos_z), colors=colors, size=size)
-                    inner_idx += 1
-                idx += num_of_obj
-
-                # =====  capture the Scenes =====
-                time.sleep(0.15)
-                im = ImageGrab.grab((8, 90, 508, 570))
-                filename = "object{num}.jpg".format(num=scenes_count)
-                scenes_count += 1
-                im.save(filename)
-                # storing data to objects
-                scene_obj = scene(filename, list_object)
-                list_scenes.append(scene_obj)
-                # reset the canvas
-                canvas.delete(self=sence)
 
     # # ===== To print the objects in each scenes =====
     # for sence in list_scenes:
