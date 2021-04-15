7ddfcd0d9edfa1362dea799e7940bce3f456c713
bird-ron <56208105+bird-ron@users.noreply.github.com>
2021-04-15 00:02:40 -0700

    Permutations and Pickle
    Changed how the scene drawer generates scenes to draw (it now generates permutations based on only size, color, and shape). Currently, the scene drawer is configured to only generate scenes of two objects. This results in 64 scenes.
    
    The scene drawer now saves the scene list to a pickle file. This means that the list of scenes should now always be loaded via pickle. We should not be running scene_drawer.py ever again.
    
    Moved functions for calculating locations from data.py to scene_drawer.py for the sake of modularity.
    
    Deleted some old test images.

--- scene_drawer.py
+++ scene_drawer.py
@@ -2,6 +2,8 @@ from model_drawer import ModelDrawer
 from vpython import canvas
 from PIL import ImageGrab
 from data import Scene
+import itertools
+import pickle
 import random
 import time
 import os
@@ -12,99 +14,109 @@ class SceneDrawer:
     screenshot_wait_time = 0.15
     screenshot_folder = "img"
     screenshot_filename = "scene"
-    images_to_generate = 10
-    min_models_per_scene = 2
-    max_models_per_scene = 4
+    pickle_folder = "pickle"
+    pickle_filename = "scene_list"
+    models_per_scene = range(2, 3)
     canvas_width = 500
     canvas_height = 500
-    dummy_index = -1
+    image_location_format = "{}/{}{}.jpg"
+    pickle_path_format = "{}/{}.pickle"
 
-    # initialize a SceneDrawer object by giving it a scene to wrap
+    # maps vpython location to pixel location
+    pixel_map = {(-2, 2, 0): (110, 86), (0, 2, 0): (250, 86), (2, 2, 0): (390, 86),
+                 (-2, 0, 0): (110, 232), (0, 0, 0): (250, 232), (2, 0, 0): (390, 232),
+                 (-2, -2, 0): (110, 378), (0, -2, 0): (250, 378), (2, -2, 0): (390, 378)}
+
     def __init__(self, my_scene):
         self.scene = my_scene
 
-    # draws the models in the scene
+    def __str__(self):
+        return str(self.scene)
+
     def draw(self):
-        for model in self.scene.model_list:
+        # (1) initialize canvas before drawing
+        my_canvas = canvas(width=SceneDrawer.canvas_width, height=SceneDrawer.canvas_height)
+        for model in self.scene.model_list:  # (2) draw every model
             model_drawer = ModelDrawer(model)
             model_drawer.draw()
+        time.sleep(SceneDrawer.screenshot_wait_time)  # (3) let vpython catch up before we screenshot
+        image_grab = ImageGrab.grab(SceneDrawer.screenshot_bounding_box)  # (4) screenshot the scene
+        image_grab.save(self.scene.image_location)
+        my_canvas.delete()  # (5) delete canvas
+
+    def assign_image_location(self, index):
+        self.scene.image_location = self.image_location_format.format(SceneDrawer.screenshot_folder,
+                                                                      SceneDrawer.screenshot_filename, index)
+
+    def assign_positions(self):
+        vpython_locations = random.sample(SceneDrawer.pixel_map.keys(), len(self.scene.model_list))
+        for model, vpython_location in zip(self.scene.model_list, vpython_locations):
+            model.vpython_location = vpython_location
+            model.normalized_location = SceneDrawer.compute_normalized_location(vpython_location)
+            model.pixel_location = SceneDrawer.compute_pixel_location(vpython_location)
+
+    @staticmethod
+    def compute_normalized_location(vpython_location):
+        normalized_x = (vpython_location[0] + 2) // 2
+        normalized_y = (-1 * vpython_location[1] + 2) // 2
+        return normalized_x, normalized_y
+
+    @staticmethod
+    def compute_pixel_location(vpython_location):
+        return SceneDrawer.pixel_map[vpython_location]
 
     @staticmethod
     def orchestrate():
-        SceneDrawer.check_screenshot_directory()
+        SceneDrawer.ensure_directory_exists(SceneDrawer.screenshot_folder)
         scene_list = SceneDrawer.generate_scenes()
-        SceneDrawer.cleanup_dummy_image(scene_list)
-        return scene_list
+        SceneDrawer.ensure_directory_exists(SceneDrawer.pickle_folder)
+        SceneDrawer.save_pickle(scene_list)
 
-    # ensures that the directory to hold screenshots exists
     @staticmethod
-    def check_screenshot_directory():
-        if not os.path.isdir(SceneDrawer.screenshot_folder):
-            os.mkdir(SceneDrawer.screenshot_folder)
+    def ensure_directory_exists(directory):
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+
+    @staticmethod
+    def generate_permutations():
+        scenes = []
+        model_permutations = ModelDrawer.generate_permutations()
+        for model_count in SceneDrawer.models_per_scene:
+            model_product = itertools.product(model_permutations, repeat=model_count)
+            for model_tuples in model_product:
+                scene = Scene()
+                scene.model_list = ModelDrawer.model_tuples_to_models(model_tuples)
+                scenes.append(scene)
+        return scenes
 
     @staticmethod
     def generate_scenes():
-        scene_list = []
-        for scene_index in range(SceneDrawer.dummy_index, SceneDrawer.images_to_generate):
-            scene_list.append(SceneDrawer.generate_random_scene(scene_index))
-            SceneDrawer.draw_scene(scene_list[-1], scene_index)
-        return scene_list
+        scenes = SceneDrawer.generate_permutations()
+        for index, my_scene in enumerate(scenes):
+            scene_drawer = SceneDrawer(my_scene)
+            scene_drawer.assign_image_location(index)
+            scene_drawer.assign_positions()
+            scene_drawer.draw()
+        return scenes
 
     @staticmethod
-    def generate_random_scene(scene_index):
-        scene = Scene()
-        scene.model_list = []
-        scene.image_location = SceneDrawer.find_image_location(scene_index)
-        model_count = random.randint(SceneDrawer.min_models_per_scene, SceneDrawer.max_models_per_scene)
-        vpython_location_set = set()  # to check for overlapping models
-        model_index = 0
-        while model_index < model_count:
-            model = ModelDrawer.generate_random_model()
-            if model.vpython_location not in vpython_location_set:
-                vpython_location_set.add(model.vpython_location)
-                scene.model_list.append(model)
-                model_index += 1
-        return scene
-
-    # a single iteration of the loop in "generate_scenes"
-    # steps:
-    # (1) initialize a new, blank canvas
-    # (2) draw the scene
-    # (3) wait for vpython to finish drawing the scene
-    # (4) screenshot the scene
-    # (5) delete the canvas
-    @staticmethod
-    def draw_scene(my_scene, index):
-        # (1) initialize canvas before drawing
-        my_canvas = canvas(width=SceneDrawer.canvas_width, height=SceneDrawer.canvas_height)
-
-        # (2) create our scene-drawing object, then draw
-        scene_drawer = SceneDrawer(my_scene)
-        scene_drawer.draw()
-
-        # (3) let vpython catch up before we screenshot
-        time.sleep(SceneDrawer.screenshot_wait_time)
-
-        # (4) screenshot the scene
-        my_scene.image_location = SceneDrawer.find_image_location(index)
-        image_grab = ImageGrab.grab(SceneDrawer.screenshot_bounding_box)
-        image_grab.save(my_scene.image_location)
-
-        # (5) delete canvas
-        my_canvas.delete()
+    def save_pickle(scene_list):
+        filename = SceneDrawer.pickle_path_format.format(SceneDrawer.pickle_folder, SceneDrawer.pickle_filename)
+        with open(filename, "wb") as pickle_file:
+            pickle.dump(scene_list, pickle_file)
 
     @staticmethod
-    def cleanup_dummy_image(scene_list):
-        os.remove(SceneDrawer.find_image_location(SceneDrawer.dummy_index))
-        del scene_list[0]
-
-    @staticmethod
-    def find_image_location(index):
-        return "{}/{}{}.jpg".format(SceneDrawer.screenshot_folder, SceneDrawer.screenshot_filename, index)
+    def load_pickle():
+        filename = SceneDrawer.pickle_path_format.format(SceneDrawer.pickle_folder, SceneDrawer.pickle_filename)
+        with open(filename, "rb") as pickle_file:
+            return pickle.load(pickle_file)
 
 
 def main():
-    return SceneDrawer.orchestrate()
+    SceneDrawer.orchestrate()
+    scene_list = SceneDrawer.load_pickle()
+    for scene in scene_list:
+        print(scene)
 
 
 if __name__ == "__main__":
