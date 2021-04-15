ee4b65f4feef3a3f8409ca5c29f8c6a36e688413
bird-ron <56208105+bird-ron@users.noreply.github.com>
2021-04-14 19:22:57 -0700

    Image Generation Code
    Uploaded new code for the Image Generation Task.

---
+++ model_drawer.py
@@ -0,0 +1,82 @@
+from vpython import box, color, sphere, vector
+from data import Model
+import random
+
+
+class ModelDrawer:
+    # map descriptions of models to values suitable for use in vpython
+    vpython_function_map = {"box": box, "sphere": sphere}
+    size_kwarg_map = {"box": "size", "sphere": "radius"}
+    size_derivation_map = None  # assign this later; needs access to static variables in the same class
+    size_map = {"small": 0.7, "big": 1.2}
+    radius_map = {"small": 0.4, "big": 0.8}
+    color_map = {"red": color.red, "green": color.green}
+
+    # initialize a model drawer by giving it a model to draw
+    def __init__(self, model):
+        self.model = model
+
+    # derive the vpython function and the kwargs that go with it, then call it
+    def draw(self):
+        vpython_function = self.derive_vpython_function()
+        vpython_kwargs = self.derive_vpython_kwargs()
+        vpython_function(**vpython_kwargs)
+
+    # match the name of the shape with the vpython function that draws it
+    # example: "box" -> box
+    def derive_vpython_function(self):
+        return self.vpython_function_map[self.model.shape]
+
+    # derive each kwarg, and put it in a dict
+    def derive_vpython_kwargs(self):
+        return {self.derive_size_kwarg(): self.call_size_derivation_function(), "color": self.derive_color(),
+                "pos": self.derive_pos()}
+
+    # match the name of the shape with the size kwarg that we need
+    # example: "box" -> "size", but "sphere" -> "radius"
+    def derive_size_kwarg(self):
+        return self.size_kwarg_map[self.model.shape]
+
+    # match the name of the shape with the function that derives its size
+    # example: "box" -> derive_size, but "sphere" -> derive_radius
+    def call_size_derivation_function(self):
+        return self.size_derivation_map[self.model.shape](self)
+
+    # derives the vector-size of a box based on a verbal description of its size
+    # example: "small" -> 0.7 -> vector(0.7, 0.7, 0.7)
+    def derive_size(self):
+        size_value = self.size_map[self.model.size]
+        size_args = [size_value] * 3
+        size = vector(*size_args)
+        return size
+
+    # derives the radius of the sphere based on a verbal description of its size
+    # example: "big" -> 0.8
+    def derive_radius(self):
+        return self.radius_map[self.model.size]
+
+    # derives the vector-color (RGB) of the model based on the name of the color
+    # example: "red" -> color.red -> vector(1, 0, 0)
+    def derive_color(self):
+        return self.color_map[self.model.color]
+
+    # derives the vector-position of the model based on its location on the 3x3 grid
+    # example: (0, 0, 0) -> vector(0, 0, 0)
+    def derive_pos(self):
+        return vector(*self.model.vpython_location)
+
+    @staticmethod
+    def generate_random_model():
+        random_model = Model()
+        random_model.size = random.choice(list(ModelDrawer.size_map.keys()))
+        random_model.color = random.choice(list(ModelDrawer.color_map.keys()))
+        random_model.shape = random.choice(list(ModelDrawer.vpython_function_map.keys()))
+        random_model.vpython_location = (random.randint(-1, 1) * 2, random.randint(-1, 1) * 2, 0)
+        random_model.derive_normalized_location()
+        random_model.derive_pixel_location()
+        return random_model
+
+
+# we are assigning a static variable that uses other static variables of the class
+# so, we can't actually perform this assignment until the class was already initialized
+ModelDrawer.size_derivation_map = {"box": ModelDrawer.derive_size, "sphere": ModelDrawer.derive_radius}
---
+++ scene_drawer.py
@@ -0,0 +1,111 @@
+from model_drawer import ModelDrawer
+from vpython import canvas
+from PIL import ImageGrab
+from data import Scene
+import random
+import time
+import os
+
+
+class SceneDrawer:
+    screenshot_bounding_box = (8, 110, 506, 608)
+    screenshot_wait_time = 0.15
+    screenshot_folder = "img"
+    screenshot_filename = "scene"
+    images_to_generate = 500
+    min_models_per_scene = 2
+    max_models_per_scene = 4
+    canvas_width = 500
+    canvas_height = 500
+    dummy_index = -1
+
+    # initialize a SceneDrawer object by giving it a scene to wrap
+    def __init__(self, my_scene):
+        self.scene = my_scene
+
+    # draws the models in the scene
+    def draw(self):
+        for model in self.scene.model_list:
+            model_drawer = ModelDrawer(model)
+            model_drawer.draw()
+
+    @staticmethod
+    def orchestrate():
+        SceneDrawer.check_screenshot_directory()
+        scene_list = SceneDrawer.generate_scenes()
+        SceneDrawer.cleanup_dummy_image(scene_list)
+        return scene_list
+
+    # ensures that the directory to hold screenshots exists
+    @staticmethod
+    def check_screenshot_directory():
+        if not os.path.isdir(SceneDrawer.screenshot_folder):
+            os.mkdir(SceneDrawer.screenshot_folder)
+
+    @staticmethod
+    def generate_scenes():
+        scene_list = []
+        for scene_index in range(SceneDrawer.dummy_index, SceneDrawer.images_to_generate):
+            scene_list.append(SceneDrawer.generate_random_scene(scene_index))
+            SceneDrawer.draw_scene(scene_list[-1], scene_index)
+        return scene_list
+
+    @staticmethod
+    def generate_random_scene(scene_index):
+        scene = Scene()
+        scene.model_list = []
+        scene.image_location = SceneDrawer.find_image_location(scene_index)
+        model_count = random.randint(SceneDrawer.min_models_per_scene, SceneDrawer.max_models_per_scene)
+        vpython_location_set = set()  # to check for overlapping models
+        model_index = 0
+        while model_index < model_count:
+            model = ModelDrawer.generate_random_model()
+            if model.vpython_location not in vpython_location_set:
+                vpython_location_set.add(model.vpython_location)
+                scene.model_list.append(model)
+                model_index += 1
+        return scene
+
+    # a single iteration of the loop in "generate_scenes"
+    # steps:
+    # (1) initialize a new, blank canvas
+    # (2) draw the scene
+    # (3) wait for vpython to finish drawing the scene
+    # (4) screenshot the scene
+    # (5) delete the canvas
+    @staticmethod
+    def draw_scene(my_scene, index):
+        # (1) initialize canvas before drawing
+        my_canvas = canvas(width=SceneDrawer.canvas_width, height=SceneDrawer.canvas_height)
+
+        # (2) create our scene-drawing object, then draw
+        scene_drawer = SceneDrawer(my_scene)
+        scene_drawer.draw()
+
+        # (3) let vpython catch up before we screenshot
+        time.sleep(SceneDrawer.screenshot_wait_time)
+
+        # (4) screenshot the scene
+        my_scene.image_location = SceneDrawer.find_image_location(index)
+        image_grab = ImageGrab.grab(SceneDrawer.screenshot_bounding_box)
+        image_grab.save(my_scene.image_location)
+
+        # (5) delete canvas
+        my_canvas.delete()
+
+    @staticmethod
+    def cleanup_dummy_image(scene_list):
+        os.remove(SceneDrawer.find_image_location(SceneDrawer.dummy_index))
+        del scene_list[0]
+
+    @staticmethod
+    def find_image_location(index):
+        return "{}/{}{}.jpg".format(SceneDrawer.screenshot_folder, SceneDrawer.screenshot_filename, index)
+
+
+def main():
+    SceneDrawer.orchestrate()
+
+
+if __name__ == "__main__":
+    main()
