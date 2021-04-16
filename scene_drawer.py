a7ce0f38d0877d2dd283d2868047ceebb89cafc3
bird-ron <56208105+bird-ron@users.noreply.github.com>
2021-04-15 17:58:59 -0700

    Image Generation, Pickle
    Generated all 4672 scenes, and saved this list of scenes to scene_list.pickle. Images are now saved using vpython's capture function instead of a screenshotting tool. Images are first saved to the downloads folder, then moved to the img folder in the project directory.
    
    The code now makes use of vpython functions to wait for vpython to finish drawing each scene, rather than using an arbitrary wait time. Additionally, the vpython default canvas is deleted since it is unused (each question makes its own canvas).

--- scene_drawer.py
+++ scene_drawer.py
@@ -1,26 +1,32 @@
 from model_drawer import ModelDrawer
-from vpython import canvas
-from PIL import ImageGrab
+from vpython import canvas, scene
 from data import Scene
 import itertools
-import pickle
 import random
+import pickle
 import time
 import os
 
 
 class SceneDrawer:
-    screenshot_bounding_box = (8, 110, 506, 608)
-    screenshot_wait_time = 0.15
-    screenshot_folder = "img"
-    screenshot_filename = "scene"
+    models_per_scene = range(2, 5)
+
+    downloads_template = os.path.join(os.getenv("USERPROFILE"), "Downloads") + "\\{image_name}"
+    cwd_template = os.getcwd() + "\\{image_location}"
+    download_wait_seconds = 1
+
+    image_folder = "img"
+    image_filename = "scene"
+    image_location_format = "{}\\{}{}.png"
+
     pickle_folder = "pickle"
     pickle_filename = "scene_list"
-    models_per_scene = range(2, 3)
-    canvas_width = 500
-    canvas_height = 500
-    image_location_format = "{}/{}{}.jpg"
-    pickle_path_format = "{}/{}.pickle"
+    pickle_path_format = "{}\\{}.pickle"
+
+    vpython_canvas_width = 500
+    vpython_canvas_height = 500
+    vpython_redraw_flag = "redraw"
+    vpython_draw_complete_flag = "draw_complete"
 
     # maps vpython location to pixel location
     pixel_map = {(-2, 2, 0): (110, 86), (0, 2, 0): (250, 86), (2, 2, 0): (390, 86),
@@ -34,19 +40,19 @@ class SceneDrawer:
         return str(self.scene)
 
     def draw(self):
-        # (1) initialize canvas before drawing
-        my_canvas = canvas(width=SceneDrawer.canvas_width, height=SceneDrawer.canvas_height)
-        for model in self.scene.model_list:  # (2) draw every model
+        image_name = SceneDrawer.filename_from_path(self.scene.image_location)
+        my_canvas = canvas(width=SceneDrawer.vpython_canvas_width, height=SceneDrawer.vpython_canvas_height)
+        for model in self.scene.model_list:
             model_drawer = ModelDrawer(model)
             model_drawer.draw()
-        time.sleep(SceneDrawer.screenshot_wait_time)  # (3) let vpython catch up before we screenshot
-        image_grab = ImageGrab.grab(SceneDrawer.screenshot_bounding_box)  # (4) screenshot the scene
-        image_grab.save(self.scene.image_location)
-        my_canvas.delete()  # (5) delete canvas
+        my_canvas.waitfor(SceneDrawer.vpython_redraw_flag)
+        my_canvas.waitfor(SceneDrawer.vpython_draw_complete_flag)
+        my_canvas.capture(image_name)
+        my_canvas.delete()
 
     def assign_image_location(self, index):
-        self.scene.image_location = self.image_location_format.format(SceneDrawer.screenshot_folder,
-                                                                      SceneDrawer.screenshot_filename, index)
+        self.scene.image_location = self.image_location_format.format(SceneDrawer.image_folder,
+                                                                      SceneDrawer.image_filename, index)
 
     def assign_positions(self):
         vpython_locations = random.sample(SceneDrawer.pixel_map.keys(), len(self.scene.model_list))
@@ -67,8 +73,9 @@ class SceneDrawer:
 
     @staticmethod
     def orchestrate():
-        SceneDrawer.ensure_directory_exists(SceneDrawer.screenshot_folder)
+        SceneDrawer.ensure_directory_exists(SceneDrawer.image_folder)
         scene_list = SceneDrawer.generate_scenes()
+        SceneDrawer.move_images(scene_list)
         SceneDrawer.ensure_directory_exists(SceneDrawer.pickle_folder)
         SceneDrawer.save_pickle(scene_list)
 
@@ -84,13 +91,14 @@ class SceneDrawer:
         for model_count in SceneDrawer.models_per_scene:
             model_product = itertools.product(model_permutations, repeat=model_count)
             for model_tuples in model_product:
-                scene = Scene()
-                scene.model_list = ModelDrawer.model_tuples_to_models(model_tuples)
-                scenes.append(scene)
+                my_scene = Scene()
+                my_scene.model_list = ModelDrawer.model_tuples_to_models(model_tuples)
+                scenes.append(my_scene)
         return scenes
 
     @staticmethod
     def generate_scenes():
+        scene.delete()  # delete built-in vpython canvas; we will make our own for each scene
         scenes = SceneDrawer.generate_permutations()
         for index, my_scene in enumerate(scenes):
             scene_drawer = SceneDrawer(my_scene)
@@ -98,6 +106,24 @@ class SceneDrawer:
             scene_drawer.assign_positions()
             scene_drawer.draw()
         return scenes
+
+    @staticmethod
+    def move_images(scene_list):
+        time.sleep(SceneDrawer.download_wait_seconds)
+        for my_scene in scene_list:
+            SceneDrawer.move_image(my_scene)
+
+    @staticmethod
+    def move_image(my_scene):
+        image_location = my_scene.image_location
+        image_name = SceneDrawer.filename_from_path(image_location)
+        downloads_image_path = SceneDrawer.downloads_template.format(image_name=image_name)
+        cwd_image_path = SceneDrawer.cwd_template.format(image_location=image_location)
+        os.rename(downloads_image_path, cwd_image_path)
+
+    @staticmethod
+    def filename_from_path(path):
+        return path[path.index("\\") + 1:]
 
     @staticmethod
     def save_pickle(scene_list):
@@ -115,8 +141,8 @@ class SceneDrawer:
 def main():
     SceneDrawer.orchestrate()
     scene_list = SceneDrawer.load_pickle()
-    for scene in scene_list:
-        print(scene)
+    for my_scene in scene_list:
+        print(my_scene)
 
 
 if __name__ == "__main__":
