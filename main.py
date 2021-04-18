4054e9c1746ac16fd5ca8351af045db8cc0675b8
bird-ron <56208105+bird-ron@users.noreply.github.com>
2021-04-18 16:20:44 -0700

    10 Scenes
    Temporarily changed main to generate only the first 10 items.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from languageExpressions import language_controller
+from createjson import createjsonfile
 from scene_drawer import SceneDrawer
 from segment import segment_images
-from createjson import createjsonfile
-if __name__ == "__main__":
-    #img folder for image generation
-    scene_list = SceneDrawer.load_pickle()
-    for sc in scene_list:
-        print(sc)
 
+
+if __name__ == "__main__":
+    # img folder for image generation
+    scene_list = SceneDrawer.load_pickle()[0:10]
     language_controller(scene_list)
-    #results folder for segemented images
+    # results folder for segemented images
     segment_images("segmented_img", scene_list)
     createjsonfile("segmented_img", scene_list, "output.json")
-
